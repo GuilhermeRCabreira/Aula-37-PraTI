5aa187ef81d6e58fbb3df4f12a5a0aa03a062b10
GuilhermeRCabreira <guilhermercabreira@gmail.com>
2024-07-20 01:08:49 -0300

    Aula 37

---
+++ Aula-37/src/App.jsx
@@ -0,0 +1,15 @@
+import TimerComponent from "./components/LifeCycle";
+import UserProfile from "./components/User";
+import NewsUpdater from "./components/NewsUpdater";
+
+function App() {
+  return (
+    <>
+      {/* <UserProfile /> */}
+      {/* <TimerComponent /> */}
+      <NewsUpdater />
+    </>
+  );
+}
+
+export default App;
---
+++ Aula-37/src/components/NewsUpdater.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+function NewsUpdater() {
+  const [news, setNews] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await response.json();
+
+        setNews(data.slice(0, 5));
+      } catch (error) {
+        console.error("Erro:", error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchNews();
+
+    const interval = setInterval(fetchNews, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Últimas Notícias</h1>
+      {isLoading ? (
+        <p>Carregando notícias</p>
+      ) : (
+        <ul>
+          {news.map((article) => (
+            <li key={article.id}>
+              <p>{article.title}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default NewsUpdater;
